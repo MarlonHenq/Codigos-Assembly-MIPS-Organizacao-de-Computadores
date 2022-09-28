c1233cb5210fe3e8fe384501e99dbd3688bc73df
MarlonHenq <marlon@marlonhenq.dev>
2022-09-28 12:51:25 -0300

    ex2 comentários

--- Lista2/mips2.asm
+++ Lista2/mips2.asm
@@ -4,22 +4,22 @@
 
 .text
 
-lw $a0, var1
-lw $a1, var2
+lw $a0, var1 # load 
+lw $a1, var2 #
 
-jal multiplica
+jal multiplica #chama a função multiplica
 
-move $s1, $v0
+move $s1, $v0 #Pega o resultado da função e taca em $s1
 
-li $v0, 1
-move $a0, $s1
-syscall 
-
-li $v0, 10
-syscall
+li $v0, 1 #Taca 1 em $v0 para dizer que será printado um inteiro
+move $a0, $s1 #Taca o valor em $a0 para a função de print pegar
+syscall #chama o sistema
+ 
+li $v0, 10 #Taca 10 em $v0 para dizer que o programa ira acabar
+syscall #chama o sistema
 
 	multiplica:
-		mult $a0, $a1
-		mflo $v0
+		mult $a0, $a1 #Pega os parâmetros e multiplica
+		mflo $v0 #Pega o resultado e taca em v0
 		
-		jr $ra
+		jr $ra #Volta da onde a função foi chamada
